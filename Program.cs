6bf1b7b13bd39df9f65ac9e1dc77dbedf54762a2
VanessaCV <erioyaki@gmail.com>
2019-11-13 12:59:30 +0100

    some correction
    done

--- Program.cs
+++ Program.cs
@@ -6,89 +6,74 @@ namespace Services_T1
     delegate void Fonsi();
     class Program
     {
+        public static int optionCount = 0;
+
         static void Main(string[] args)
         {
-            int numOptions = 3;
-            int totalSize = numOptions + 2;
+            int numOptions = 0;
+            Fonsi[] myFonsi = new Fonsi[numOptions];
 
-            //Fonsi myFonsi = new Fonsi(outOption);
-            //myFonsi += option1;
-            //myFonsi += option2;
-            //myFonsi += option3;
-            //myFonsi += defOption;
-
-            Fonsi[] myFonsi = new Fonsi[] { outOption, option1, option2, option3, defOption };
-
-            string[] options = new string[totalSize];
-            for (int i = 0; i < options.Length; i++)
+            do
             {
-                if (i == 0)
+                Console.WriteLine("How many options do you want?");
+                string optionNumberStr = Console.ReadLine();
+                if (Int32.TryParse(optionNumberStr, out numOptions))
                 {
-                    options[i] = 
-                        "___________________________" +
-                        "\r\nMENU"+
-                        "\r\n___________________________";
+
+                    myFonsi = new Fonsi[numOptions];
+
+                    for (int i = 0; i <  numOptions; i++, optionCount++)
+                    {
+                        myFonsi[i] = option;
+                    }
                 }
-                else if (i == options.Length-1)
-                {
-                    options[i] = "Press 0 to exit";
-                }
-                else
-                {
-                    options[i] = "Press "+i+".-";
-                }
+
+            } while (numOptions<=0);
+
+            string[] options = new string[numOptions];
+            for (int i = 1; i <= options.Length; i++)
+            {
+                options[i-1] = "Option "+i;
             }
+
             MenuGenerator(options, myFonsi);
             Console.ReadKey();
         }
 
-        //public static void MenuGenerator(string [] options, Fonsi myFonsi)
         public static void MenuGenerator(string [] options, Fonsi []myFonsi)
         {
-            if(options.Length == myFonsi.Length){
-
-                string option = "0";
-                do
-                {
-                    for (int i = 0; i < options.Length; i++)
-                    {
-                        Console.WriteLine(options[i]);
-                    }
-
-                    option = Console.ReadLine();
-                
-                    if (!Int32.TryParse(option, out int aux) || ( aux < 0 || aux > 4))
-                    {
-                        aux = 4;
-                    }
-
-                    myFonsi[aux]();
-
-                    //Delegate[] test = myFonsi.GetInvocationList();
-                    //test[option];
-
-                } while (option!="0");
-            }
-            else
+            string option = "0";
+            do
             {
-                Console.WriteLine("El menú no es coherente con el delegado");
-            }
+                Console.WriteLine("Choose an option: ");
+                foreach (string str in options)
+                {
+                    Console.WriteLine(str);
+                }
+                Console.WriteLine("Press 0 to exit.");
+
+                option = Console.ReadLine();
+
+                if (Int32.TryParse(option, out int intOption) && intOption < myFonsi.Length && intOption != 0)
+                {
+                    myFonsi[intOption]();
+                }
+                else if(option == "0")
+                {
+                    Console.WriteLine("Leaving . . . ");
+                }
+                else
+                {
+                    Console.WriteLine("There was an error with the option given. Please, choose some other option.");
+                }
+
+            } while (option!="0");
+
         }
 
-        public static void option1(){
-            Console.WriteLine("You've chosen 1");
-        }
-        public static void option2(){
-            Console.WriteLine("You've chosen 2");
-        }
-        public static void option3(){
-            Console.WriteLine("You've chosen 3");
-        }
-        public static void defOption(){
-            Console.WriteLine("Option not avaiable.");
-        }
-        public static void outOption(){
-            Console.WriteLine("Leaving . . .");
+        public static void option(){
+            char a = (char)('1'+optionCount);
+            Console.WriteLine("You've chosen "+a);
         }
     }
 }
